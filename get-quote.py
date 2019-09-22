b2a6e2aa59c10e94548481a0fb8b9f0e089f82c2
Prashant SIngh <singh08prashant@gmail.com>
2019-09-22 23:07:58 +0530

    Added more quotes, prints 2 of them

--- get-quote.py
+++ get-quote.py
@@ -5,10 +5,11 @@ def primary():
   f = open("quotes.txt")
   quotes = f.readlines()
   f.close()
-  last = 13
-  rnd = random.randint(0, last)
+  last = 15
+  rnd1 = random.randint(0, last)
+  rnd2 = random.randint(0, last)
 
-  print(quotes[rnd])
+  print(quotes[rnd1],quotes[rnd2])
 
 if __name__== "__main__":
   primary()
